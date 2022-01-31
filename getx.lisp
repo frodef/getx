1d0377033efc3db78da94331afae37687d67bb4e
Frode V. Fjeld <frodevf@gmail.com>
2022-01-31 01:43:05 +0100

    *** empty log message ***

--- getx.lisp
+++ getx.lisp
@@ -8,8 +8,9 @@
   "Query a hierarchical data-structure X by recursively applying
 INDICATORS sequentially.
 
-If INDICATOR is an atom (typically a keyword) it is either looked up
- in X by CL:GETF or CL:GETHASH, depending on the type of X.
+If INDICATOR is an atom (typically a symbol) it is either looked up in
+X by CL:GETF, CL:GETHASH, or CL:SLOT-VALUE, depending on the type of
+X.
 
 Otherwise, INDICATOR is a special indicator form that can operate on
 any number of data types for X. ~@[ See the documentation for the
@@ -30,18 +31,26 @@ employee in some Acme company whose first name is Frode:
      (apply #'?
 	    (gethash (car indicators) x)
 	    (cdr indicators)))
+    ((typep x '(or standard-object structure-object))
+     (apply #'?
+	    (slot-value x (car indicators))
+	    (cdr indicators)))
     (t (apply #'?
-	      (getx x (car indicators))
+	      (getf x (car indicators))
 	      (cdr indicators)))))
 
 (defmethod documentation ((x (eql '?)) (doc-type (eql 'function)))
+  "Inject special indicators into docstring for GETX:?"
   (format nil (documentation #'? 'function) *special-getx-operators*))
 
-(defmacro define-getx (name getter-lambda &body body)
-  (let ((getter-name (intern (format nil "%~A" name))))
+(defmacro define-getx (name lambda &body body)
+  "Define a special indicator. This consists of two functions: The
+query function that performs the relevant query, and the specifier
+function that merely records the indicator for perusal by GETX:?"
+  (let ((doquery-name (intern (format nil "%~A" name))))
     `(progn
        (pushnew ',name *special-getx-operators*)
-       (defun ,getter-name (continuation ,@getter-lambda)
+       (defun ,doquery-name (continuation ,@lambda)
 	 (flet ((proceed (new-plist)
 		  (apply #'? new-plist continuation)))
 	   ,@(if (stringp (first body))
@@ -50,39 +59,39 @@ employee in some Acme company whose first name is Frode:
        (defun ,name (&rest args)
 	 ,@(when (stringp (first body))
 	     (list (concatenate 'string "Special indicator: " (first body))))
-	 (list* ',getter-name args)))))
+	 (list* ',doquery-name args)))))
 
-(define-getx index (plist n)
-  "Take the Nth element of PLIST."
+(define-getx index (sequence n)
+  "Take the Nth element of SEQUENCE, or NIL if out of range."
   (proceed
-   (etypecase plist
+   (etypecase sequence
      (list
-      (nth n plist))
+      (nth n sequence))
      (vector
-      (when (<= 0 n (length plist))
-	(aref plist n))))))
+      (when (<= 0 n (length sequence))
+	(aref sequence n))))))
 
-(define-getx seek (plist indicator value &optional (test 'equal))
-  "Find the first element of PLIST where INDICATOR matches VALUE under TEST."
-  (dolist (sub-plist plist)
-    (when (funcall test value (? sub-plist indicator))
-      (return (proceed sub-plist)))))
+(define-getx seek (list indicator value &optional (test 'equal))
+  "Find the first element of LIST where INDICATOR matches VALUE under TEST."
+  (dolist (element list)
+    (when (funcall test value (? element indicator))
+      (return (proceed element)))))
 
-(define-getx select (plists indicator value &optional (test 'equal))
-  "Select each plist in PLISTS where INDICATOR matches VALUE under TEST. Returns a list of plists."
-  (mapcan (lambda (plist)
-	    (when (funcall test value (? plist indicator))
-	      (list (proceed plist))))
-	  plists))
+(define-getx select (list indicator value &optional (test 'equal))
+  "Select each element of LIST where INDICATOR matches VALUE under TEST."
+  (mapcan (lambda (element)
+	    (when (funcall test value (? element indicator))
+	      (list (proceed element))))
+	  list))
 
-(define-getx prog? (plist &rest indicators)
+(define-getx prog? (x &rest indicators)
   "Process a sequence of INDICATORS."
-  (proceed (apply #'? plist indicators)))
+  (proceed (apply #'? x indicators)))
 
-(define-getx multiple (plist &rest indicators)
-  "Return a list from processing multiple INDICATORS."
+(define-getx multiple (x &rest indicators)
+  "Return a list, being multiple INDICATORS from X."
   (mapcar (lambda (indicator)
-	    (proceed (? plist indicator)))
+	    (proceed (? x indicator)))
 	  indicators))
 
 (define-getx each (plist each-key)
@@ -92,26 +101,31 @@ returning the first value for KEY)."
 	when (eq key each-key)
 	  collect (proceed value)))
 
-(define-getx each-key (plist)
-  "Return the keys of PLIST, discarding the values."
-  (etypecase plist
+(define-getx each-key (x)
+  "Return the keys of a plist or hash-table X, discarding the values."
+  (etypecase x
     (list
-     (loop for (key value) on plist by #'cddr
+     (loop for (key value) on x by #'cddr
 	   collect (proceed key)))
     (hash-table
-     (loop for key being the hash-keys of plist
+     (loop for key being the hash-keys of x
 	   collect key))))
 
-(define-getx each-value (plist)
+(define-getx each-value (x)
   "Return the values of PLIST, discarding the keys."
-  (loop for (key value) on plist by #'cddr
-	collect (proceed value)))
+  (etypecase x
+    (list
+     (loop for (key value) on x by #'cddr
+	   collect (proceed value)))
+    (hash-table
+     (loop for value being the hash-values of x
+	   collect value))))
 
-(define-getx either (plist &rest indicators)
+(define-getx either (x &rest indicators)
   "Return the first INDICATOR that returns a non-NIL result."
   (loop for indicator in indicators
-	thereis (proceed (? plist indicator))))
+	thereis (proceed (? x indicator))))
 
-(define-getx slot (object slot-name)
-  "Return the slot of an object."
-  (proceed (slot-value object slot-name)))
+(define-getx associate (alist item &key (key 'identity) (test 'eq))
+  "Look up ITEM in ALIST as by CL:ASSOC."
+  (proceed (assoc item alist :key key :test test)))
