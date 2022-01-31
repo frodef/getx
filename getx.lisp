3e0b05e4944c23d8eeff12ef6446fc66b7f9ca09
Frode V. Fjeld <frodevf@gmail.com>
2022-01-31 13:01:10 +0100

    Explicit QUERY-LAMBDA. Inlining.

--- getx.lisp
+++ getx.lisp
@@ -4,6 +4,7 @@
 (defparameter *special-getx-operators* nil
   "Records a list of operators defined by DEFINE-GETX.")
 
+(declaim (inline ?))
 (defun ? (x &rest indicators)
   "Query a hierarchical data-structure X by recursively applying
 INDICATORS sequentially.
@@ -38,32 +39,63 @@ employee in some Acme company whose first name is Frode:
     (t (apply #'?
 	      (getf x (car indicators))
 	      (cdr indicators)))))
+(declaim (notinline ?))
 
 (defmethod documentation ((x (eql '?)) (doc-type (eql 'function)))
   "Inject special indicators into docstring for GETX:?"
   (format nil (documentation #'? 'function) *special-getx-operators*))
 
-(defmacro define-getx (name lambda-list &body body)
+(defmacro define-getx (name surface-lambda &body options-body)
   "Define a GETX special indicator. This consists of two functions:
-The query function DOQUERY-<NAME> that performs the relevant query,
+The query function %<NAME> that performs the relevant query,
 and the specifier function <NAME> that merely records the indicator
 for perusal by GETX:?
 
-In LAMBDA-LIST, the first argument is the current object X, and the
+In SURFACE-LAMBDA, the first argument is the current object X, and the
 remaining arguments relate to the indicator."
-  (let ((doquery-name (intern (format nil "%~A" name))))
-    `(progn
-       (pushnew ',name *special-getx-operators*)
-       (defun ,doquery-name (continuation ,@lambda-list)
-	 (flet ((proceed (new-plist)
-		  (apply #'? new-plist continuation)))
-	   ,@(if (stringp (first body))
-		 (cdr body)
-		 body)))
-       (defun ,name (&rest args)
-	 ,@(when (stringp (first body))
-	     (list (concatenate 'string "Special indicator: " (first body))))
-	 (list* ',doquery-name args)))))
+  (let* ((do-query-name
+	     (intern (format nil "%~A" name)))
+	 (body
+	   (loop for b = options-body then (cddr b)
+		   thereis (unless (keywordp (first b)) b)))
+	 (options
+	   (ldiff options-body body)))
+    (destructuring-bind (&key (query-lambda surface-lambda))
+	options
+      (assert (and (every #'symbolp query-lambda)
+		   (null (intersection query-lambda lambda-list-keywords)))
+	      (query-lambda)
+	      "The ~S must only include required arguments: ~S" 'query-lambda query-lambda)
+      (assert (eq (first surface-lambda)
+		  (first query-lambda))
+	      (surface-lambda query-lambda)
+	      "The ~S and ~S must share the same first argument (for the object X)."
+	      'surface-lambda 'query-lambda)
+      `(progn
+	 (pushnew ',name *special-getx-operators*)
+	 (declaim (inline ,name ,do-query-name))
+	 (defun ,do-query-name (continuation ,@query-lambda)
+	   (flet ((proceed (new-plist)
+		    (apply #'? new-plist continuation)))
+	     ,@(if (stringp (first body))
+		   (cdr body)
+		   body)))
+	 (defun ,name ,(cdr surface-lambda)
+	   ,@(when (stringp (first body))
+	       (list (concatenate 'string "Special GETX indicator: " (first body))))
+	   (list ',do-query-name ,@(cdr query-lambda)))
+	 (declaim (notinline ,name ,do-query-name))))))
+
+(defmacro ?? (x &rest indicators)
+  `(locally (declare (inline ? ,@*special-getx-operators*))
+     (? ,x ,@indicators)))
+
+(define-getx seek (list indicator value &optional (test 'equal))
+  :query-lambda (list indicator value test)
+  "Find the first element of LIST where INDICATOR matches VALUE under TEST."
+  (dolist (element list)
+    (when (funcall test value (? element indicator))
+      (return (proceed element)))))
 
 (define-getx index (sequence n)
   "Take the Nth element of SEQUENCE, or NIL if out of range."
@@ -75,13 +107,8 @@ remaining arguments relate to the indicator."
       (when (<= 0 n (length sequence))
 	(aref sequence n))))))
 
-(define-getx seek (list indicator value &optional (test 'equal))
-  "Find the first element of LIST where INDICATOR matches VALUE under TEST."
-  (dolist (element list)
-    (when (funcall test value (? element indicator))
-      (return (proceed element)))))
-
 (define-getx select (list indicator value &optional (test 'equal))
+  :query-lambda (list indicator value test)
   "Select each element of LIST where INDICATOR matches VALUE under TEST."
   (mapcan (lambda (element)
 	    (when (funcall test value (? element indicator))
@@ -89,10 +116,12 @@ remaining arguments relate to the indicator."
 	  list))
 
 (define-getx prog? (x &rest indicators)
+  :query-lambda (x indicators)
   "Process a sequence of INDICATORS."
   (proceed (apply #'? x indicators)))
 
 (define-getx multiple (x &rest indicators)
+  :query-lambda (x indicators)
   "Return a list, being multiple INDICATORS from X."
   (mapcar (lambda (indicator)
 	    (proceed (? x indicator)))
@@ -126,11 +155,13 @@ returning the first value for KEY)."
 	   collect value))))
 
 (define-getx either (x &rest indicators)
+  :query-lambda (x indicators)
   "Return the first INDICATOR that returns a non-NIL result."
   (loop for indicator in indicators
 	thereis (proceed (? x indicator))))
 
 (define-getx associate (alist item &key (key 'identity) (test 'eq))
+  :query-lambda (alist item key test)
   "Look up ITEM in ALIST as by CL:ASSOC."
   (proceed (assoc item alist :key key :test test)))
 
